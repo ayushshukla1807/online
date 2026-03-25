8a2fa5a25a199cbc31b2cd361ccdf64de35fef68
Sahil Gautam <sahil.gautam@collabora.com>
2026-03-25 12:13:05 +0530

    dynamic-zoom: recalculate slide zoom on resize
    Previous patches handled impress similar to writer, but both are quite
    different. In impress we want the slide to change zoom (size) as we
    resize the window.
    
    Signed-off-by: Sahil Gautam <sahil.gautam@collabora.com>
    Change-Id: I03693e622682fb718d89d4b597806d7e54674b4b

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -3181,6 +3181,8 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 	},
 
 	recalculateZoomOnResize: function() {
+		/* in impress, we always recalculate zoom on resize to keep the slide in-view.
+		 * so the second condition is unnecessary, but we keep it for consistency. */
 		if (this.isWriter() || this.isImpress())
 			this._invalidateZoomFirstFit = true;
 	},
@@ -3223,6 +3225,8 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 			bringCommentsIntoView = true;
 			this._includedCommentsInFirstFit = true;
 			this._firstFitDone = false;
+		} else if (this.isImpress()) {
+			recalcFirstFit = true;
 		}
 
 		// `recalcFirstFit` is used to recalculate/reset the zoom levels to the
@@ -3244,7 +3248,7 @@ window.L.CanvasTileLayer = window.L.Layer.extend({
 		var ratio = newSize.x / documentWidth;
 		var zoom = this._map.getScaleZoom(ratio);
 
-		if (!this._firstFitDone && this.isImpress()) {
+		if (this.isImpress()) {
 			/* assume that this is the range of diagonal sizes we are going to get
 			 * for the window and find where the current window size lies in these,
 			 * clamping it between [0,1]. then find appropriate margin between 4-9%
